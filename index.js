26d0eb686c6ba926f28cc6e65bffca0db4deb2bd
Sanjay Thakur <sanjaythakur361899@gmail.com>
2023-01-12 17:54:46 +0530

    Crud in Node Js

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const fs = require("fs");
 const path=require("path");
 const dirname=path.join(__dirname,'files');
-for(i=0;i<5;i++){
-    fs.writeFileSync(`${dirname}/hello${i}.txt`,"a simple file text file")
-}
+const filepath=`${dirname}`
+
+//for writing text file into folder named as files:
+
+// for(i=0;i<5;i++){
+//     fs.writeFileSync(`${dirname}/hello${i}.txt`,"a simple file text file")
+// }
+
+// read directory files
+
+// fs.readdir(filepath,(err,item)=>{
+//     item.forEach((items)=>console.log(items))})
+
+//read file
+
+// fs.readFile(`${filepath}/hello1.txt`,'utf8',(err,item)=>{console.log(item);})
+
+//UPDATE FILE
+
+// fs.appendFile(`${filepath}/hello0.txt`,'and this is the text we should add in the text file hello0.txt',(err)=>{ if(!err) console.log("file is updated")})
+
+
+//rename file
+// fs.rename(`${filepath}/hello0.txt`,`${filepath}/hello_rename`,(err)=>{
+//     if(!err) console.log("file is renamed")
+// })
+
+//delete file 
+
+// fs.unlinkSync(`${dirname}/hello_rename`)
